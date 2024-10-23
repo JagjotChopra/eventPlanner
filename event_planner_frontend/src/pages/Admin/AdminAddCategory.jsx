a1a4321725c65a50f36c7623d6f2d191f298f5d1
unknown <jagjotchopra@gmail.com>
2024-10-23 17:29:15 -0400

    SCRUM 67-Connect a event category form with backend API  - Frontend

--- event_planner_frontend/src/pages/Admin/AdminAddCategory.jsx
+++ event_planner_frontend/src/pages/Admin/AdminAddCategory.jsx
@@ -1,5 +1,6 @@
 // components/AddCategoryForm.js
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 const AdminAddCategory = () => {
   const [name, setName] = useState('');
@@ -13,7 +14,60 @@ const AdminAddCategory = () => {
 
   const handleSubmit = async (e) => {
 
-  }
+    e.preventDefault();
+    setError({ status: false, message: "" })
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('image', image);
+
+    const token = localStorage.getItem('token');
+    try {
+      const response = await axios.post('http://localhost:9000/api/v1/admin/EventCategory', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+      console.log(response);
+      if (response.status == "201") {
+        alert("New Event Category is Added Successfully");
+        setName('');
+        setDescription('');
+        setImage(null);
+      }
+    } catch (error) {
+      console.error('Error adding category:', error);
+      // Handle different response statuses
+      if (error.response) {
+        const { status } = error.response;
+        let message;
+
+        // Set messages based on response status
+        switch (status) {
+          case 401:
+            message = "Invalid token or no token provided.";
+            break;
+          case 403:
+            message = "Access denied. You do not have permission to perform this action.";
+            break;
+          default:
+            message = "An error occurred.";
+            break;
+        }
+
+        alert("Need To Login Again"); // Show the message to the user
+        localStorage.removeItem('token');
+        navigate('/login');
+     
+      } else {
+        setError({ status: true, message: "Server is Down. Please Try Later" });
+      }
+
+
+      // setError({status:true,message:"Error Occured"})
+    }
+  };
 
   return (
 
@@ -44,6 +98,8 @@ const AdminAddCategory = () => {
           </div>
 
           <button type="submit" className='submit-btn' style={{ marginTop: '40px', marginBottom: '40px' }}>Add New Category</button>
+          {error.status == true ? <p style={{ color: 'red' }}>{error.message}</p> : null}
+       
         </form>
 
       </div>
