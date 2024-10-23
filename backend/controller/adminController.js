352b2790f0837825adeddb34e2317e1848d926b3
Dhruvil Mody <dhruvilmody98@gmail.com>
2024-10-23 15:38:40 -0400

    Scrum-74 Created an API Controller which delete the existing events data - Backend

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -81,7 +81,12 @@ async function updateEventCategory (req, res) {
 
 
 async function deleteEventCategory (req, res) {
-    
+    try {
+        await EventCategory.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Category deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 module.exports={addNewEventCategory,deleteEventCategory,updateEventCategory,getAllEventCategory,upload};
